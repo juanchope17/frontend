5c101ea02ce6a4a0e121cfb5a0e735e0ed54f049
Xoower20 <juan.vargas17@est.iudigital.edu.co>
2024-10-13 12:13:53 -0500

    ENV

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
   server: {
-    port: 4321, // Cambia este número al puerto que prefieras
-  }
-})
-
+    port: process.env.PORT, // Cambia este número al puerto que prefieras
+  },
+});
